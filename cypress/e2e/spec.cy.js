9c893ef01fb78b1cf0ae82d428a8c7b49d8ab403
Arden Ranta <arranta20@gmail.com>
2024-01-02 10:57:40 -0800

    initiate first cypress test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,32 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+//page load
+describe('calls the API', () => {
+  it('shows the reservation cards from the fetch', () => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/reservations', {
+      fixture: 'apiResponse1',
+    }).as('apiRequest1');
+    cy.visit('/');
+    cy.wait('@apiRequest1');
+    cy.get('.card').should('contain', 'Christie');
+    cy.get('.card').should('contain', 'Eric');
   })
 })
+
+// describe(() => {
+//   it(() => {
+
+//   })
+// })
+
+//submission form
+
+describe(() => {
+  it(() => {
+
+  })
+})
+
+// describe(() => {
+//   it(() => {
+
+//   })
+// })
